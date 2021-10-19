5033e6971242e3af734abd8dba4d5e4db649e486
Phuong Nguyen <phuong@near.org>
2021-10-19 16:29:04 -0700

    Renamerunner to workspaces for spooning example

--- examples/src/spooning.rs
+++ examples/src/spooning.rs
@@ -1,7 +1,7 @@
 use std::convert::TryInto;
 
-use runner::borsh::{self, BorshDeserialize, BorshSerialize};
-use runner::{AccountId, InMemorySigner};
+use workspaces::borsh::{self, BorshDeserialize, BorshSerialize};
+use workspaces::{AccountId, InMemorySigner};
 
 const STATUS_MSG_WASM_FILEPATH: &str = "./examples/res/status_message.wasm";
 
@@ -10,7 +10,7 @@ const STATUS_MSG_WASM_FILEPATH: &str = "./examples/res/status_message.wasm";
 ///
 /// If you'd like a different account to deploy it to, run the following:
 /// ```norun
-/// #[runner::main(testnet)]
+/// #[workspaces::main(testnet)]
 /// async fn deploy_testnet() {
 ///     let (contract_id, _) = deploy_status_contract("hello from testnet").await;
 ///     println!("{}", contract_id);
@@ -30,9 +30,9 @@ struct StatusMessage {
 }
 
 async fn deploy_status_contract(msg: &str) -> (AccountId, InMemorySigner) {
-    let (contract_id, signer) = runner::dev_deploy(STATUS_MSG_WASM_FILEPATH).await.unwrap();
+    let (contract_id, signer) = workspaces::dev_deploy(STATUS_MSG_WASM_FILEPATH).await.unwrap();
 
-    runner::call(
+    workspaces::call(
         &signer,
         contract_id.clone(),
         contract_id.clone(),
@@ -50,17 +50,17 @@ async fn deploy_status_contract(msg: &str) -> (AccountId, InMemorySigner) {
     (contract_id, signer)
 }
 
-#[runner::basic]
+#[workspaces::basic]
 async fn main() -> anyhow::Result<()> {
     // Grab STATE from the testnet status_message contract. This contract contains the following data:
     //   get_status(dev-20211013002148-59466083160385) => "hello from testnet"
-    let (testnet_contract_id, status_msg) = runner::scope("testnet", async {
+    let (testnet_contract_id, status_msg) = workspaces::scope("testnet", async {
         let contract_id: AccountId = TESTNET_PREDEPLOYED_CONTRACT_ID
             .to_string()
             .try_into()
             .unwrap();
 
-        let mut state_items = runner::view_state(contract_id.clone(), None).await.unwrap();
+        let mut state_items = workspaces::view_state(contract_id.clone(), None).await.unwrap();
         let state = state_items.remove("STATE").unwrap();
         let status_msg: StatusMessage =
             StatusMessage::try_from_slice(&state).expect("Expected to retrieve state");
@@ -70,12 +70,12 @@ async fn main() -> anyhow::Result<()> {
     .await?;
 
     // Patch testnet STATE into our local sandboxed status_message contract
-    runner::scope("sandbox", async move {
+    workspaces::scope("sandbox", async move {
         // Deploy with the following status_message state: sandbox_contract_id => "hello from sandbox"
         let (sandbox_contract_id, _) = deploy_status_contract("hello from sandbox").await;
 
         // Patch our testnet STATE into our local sandbox:
-        let _outcome = runner::patch_state(
+        let _outcome = workspaces::patch_state(
             sandbox_contract_id.clone(),
             "STATE".to_string(),
             &status_msg,
@@ -84,7 +84,7 @@ async fn main() -> anyhow::Result<()> {
         .unwrap();
 
         // Now grab the state to see that it has indeed been patched:
-        let result = runner::view(
+        let result = workspaces::view(
             sandbox_contract_id.clone(),
             "get_status".into(),
             serde_json::json!({
@@ -101,7 +101,7 @@ async fn main() -> anyhow::Result<()> {
         assert_eq!(status, "hello from testnet".to_string());
 
         // See that sandbox state was overriden. Grabbing get_status(sandbox_contract_id) should yield Null
-        let result = runner::view(
+        let result = workspaces::view(
             sandbox_contract_id.clone(),
             "get_status".into(),
             serde_json::json!({
--- workspaces/src/exports.rs
+++ workspaces/src/exports.rs
@@ -2,6 +2,6 @@ pub use near_crypto::{InMemorySigner, PublicKey, Signer};
 pub use near_primitives::borsh;
 pub use near_primitives::types::AccountId;
 
-/// Allow users to use `#[runner::basic]` to not use any kind of NEAR runtimes
+/// Allow users to use `#[workspaces::basic]` to not use any kind of NEAR runtimes
 /// and just purely use the underlying "basic" runtime to run tasks instead.
 pub use tokio::main as basic;
