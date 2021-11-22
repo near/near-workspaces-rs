34b3dad0c36ecbb3c63d97a16027d2c02d87c671
Phuong Nguyen <phuong@near.org>
2021-11-22 23:49:03 +0000

    Added sandbox Network impl

--- workspaces/src/network/sandbox.rs
+++ workspaces/src/network/sandbox.rs
@@ -1,4 +1,4 @@
-use std::path::Path;
+use std::{fs::File, io::Read, path::Path, str::FromStr};
 
 use async_trait::async_trait;
 
@@ -7,15 +7,22 @@ use near_primitives::types::AccountId;
 
 use crate::{NEAR_BASE, rpc::client::Client, runtime::local::SandboxServer};
 
-use super::{Account, AllowDevAccountCreation, CallExecution, TopLevelAccountCreator};
+use super::{Account, AllowDevAccountCreation, CallExecution, Network, NetworkActions, NetworkInfo, TopLevelAccountCreator};
 
 
-struct Sandbox {
+pub struct Sandbox {
     server: SandboxServer,
     client: Client,
 }
 
-impl Sandbox {
+impl Default for Sandbox {
+    fn default() -> Self {
+        let mut server = SandboxServer::default();
+        server.start().unwrap();
+
+        let client = Client::new(server.rpc_addr());
+        Self { server, client }
+    }
 }
 
 impl AllowDevAccountCreation for Sandbox {}
@@ -23,10 +30,55 @@ impl AllowDevAccountCreation for Sandbox {}
 #[async_trait]
 impl TopLevelAccountCreator for Sandbox {
     async fn create_tla(&self, id: AccountId, pk: PublicKey) -> anyhow::Result<CallExecution<Account>> {
-        todo!()
+        let root_signer = crate::runtime::local::root_account();
+        let outcome = self.client.create_account(&root_signer, id, pk, NEAR_BASE).await?;
+
+        Ok(CallExecution {
+            result: Account {},
+            details: outcome.into(),
+        })
     }
 
     async fn create_tla_and_deploy<P: AsRef<Path> + Send + Sync>(&self, id: AccountId, pk: PublicKey, wasm: P) -> anyhow::Result<CallExecution<Account>> {
+        // TODO: async_compat/async version of File
+        let mut code = Vec::new();
+        File::open(wasm)?.read_to_end(&mut code)?;
+
+        let root_signer = crate::runtime::local::root_account();
+        let outcome = self.client.create_account_and_deploy(&root_signer, id, pk, NEAR_BASE, code).await?;
+
+        Ok(CallExecution {
+            result: Account {},
+            details: outcome.into(),
+        })
+    }
+}
+
+impl NetworkActions for Sandbox {
+
+}
+
+impl NetworkInfo for Sandbox {
+    fn name(&self) -> String {
+        "Sandbox".into()
+    }
+
+    fn root_account_id(&self) -> AccountId {
+        AccountId::from_str("test.near").unwrap()
+    }
+
+    fn keystore_path(&self) -> std::path::PathBuf {
+        // std::path::PathBuf::from("")
+        todo!()
+    }
+
+    fn rpc_url(&self) -> String {
+        self.server.rpc_addr()
+    }
+
+    fn helper_url(&self) -> String {
         todo!()
     }
 }
+
+impl Network for Sandbox {}
