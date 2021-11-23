f92259040e5afff41850df8ccedd14d0f6d8a5af
Phuong Nguyen <phuong@near.org>
2021-11-23 23:14:31 +0000

    nft example now using new API

--- examples/src/nft.rs
+++ examples/src/nft.rs
@@ -1,60 +1,56 @@
 use serde_json::json;
 
-use workspaces::*;
+use workspaces::prelude::*;
 
 const NFT_WASM_FILEPATH: &str = "./examples/res/non_fungible_token.wasm";
 
-#[workspaces::main(sandbox)]
+#[tokio::main]
 async fn main() {
-    let (contract_id, signer) = dev_deploy(NFT_WASM_FILEPATH).await.unwrap();
+    let worker = workspaces::sandbox();
+    let contract = worker.dev_deploy(NFT_WASM_FILEPATH).await.unwrap();
 
-    let outcome = call(
-        &signer,
-        contract_id.clone(),
-        contract_id.clone(),
-        "new_default_meta".to_string(),
-        format!("{{\"owner_id\": \"{}\"}}", contract_id).into(),
-        None,
-    )
-    .await
-    .unwrap();
+    let outcome = worker
+        .call(
+            &contract,
+            "new_default_meta".to_string(),
+            format!("{{\"owner_id\": \"{}\"}}", contract.id()).into(),
+            None,
+        )
+        .await
+        .unwrap();
     println!("new_default_meta outcome: {:#?}", outcome);
 
     let deposit = 10000000000000000000000;
-    let outcome = call(
-        &signer,
-        contract_id.clone(),
-        contract_id.clone(),
-        "nft_mint".to_string(),
-        json!({
-            "token_id": "0",
-            "token_owner_id": contract_id,
-            "token_metadata": {
-                "title": "Olympus Mons",
-                "dscription": "Tallest mountain in charted solar system",
-                "copies": 1,
-            },
-        })
-        .to_string()
-        .into_bytes(),
-        Some(deposit),
-    )
-    .await
-    .unwrap();
+    let outcome = worker
+        .call(
+            &contract,
+            "nft_mint".to_string(),
+            json!({
+                "token_id": "0",
+                "token_owner_id": contract.id(),
+                "token_metadata": {
+                    "title": "Olympus Mons",
+                    "dscription": "Tallest mountain in charted solar system",
+                    "copies": 1,
+                },
+            })
+            .to_string()
+            .into_bytes(),
+            Some(deposit),
+        )
+        .await
+        .unwrap();
     println!("nft_mint outcome: {:#?}", outcome);
 
-    let call_result = view(
-        contract_id.clone(),
-        "nft_metadata".to_string(),
-        Vec::new().into(),
-    )
-    .await
-    .unwrap();
+    let result = worker
+        .view(contract.id(), "nft_metadata".to_string(), Vec::new().into())
+        .await
+        .unwrap();
 
     println!(
         "--------------\n{}",
-        serde_json::to_string_pretty(&call_result).unwrap()
+        serde_json::to_string_pretty(&result).unwrap()
     );
 
-    println!("Dev Account ID: {}", contract_id);
+    println!("Dev Account ID: {}", contract.id());
 }
