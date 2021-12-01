c05eb7b6385e109b642f835705e2f7d6499af0b8
Phuong Nguyen <phuong@near.org>
2021-12-01 22:59:41 +0000

    Do not expose KeyType

--- workspaces/src/lib.rs
+++ workspaces/src/lib.rs
@@ -6,7 +6,7 @@ mod worker;
 pub mod prelude;
 
 pub use network::{Contract, DevNetwork, Network};
-pub use types::{AccountId, InMemorySigner, KeyType};
+pub use types::{AccountId, InMemorySigner};
 pub use worker::{sandbox, testnet, with_sandbox, with_testnet, Worker};
 
 /// Export Borsh types so others can make use of Borsh(De)Serialize
