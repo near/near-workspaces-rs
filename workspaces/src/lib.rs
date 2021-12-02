1c131fd40965bf8a464a418a4b74472ccd393d2a
Phuong Nguyen <phuong@near.org>
2021-12-02 00:43:03 +0000

    Do not expose internal borsh

--- workspaces/src/lib.rs
+++ workspaces/src/lib.rs
@@ -8,6 +8,3 @@ pub mod prelude;
 pub use network::{Contract, DevNetwork, Network};
 pub use types::{AccountId, InMemorySigner};
 pub use worker::{sandbox, testnet, with_sandbox, with_testnet, Worker};
-
-/// Export Borsh types so others can make use of Borsh(De)Serialize
-pub use borsh;
