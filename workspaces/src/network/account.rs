70b0ada808c3dfc076e1255fede4679a3fb708d3
Phuong Nguyen <phuong@near.org>
2021-12-10 23:09:11 +0000

    Added helper functions for transfer_near, delete_account, view

--- workspaces/src/network/account.rs
+++ workspaces/src/network/account.rs
@@ -1,4 +1,4 @@
-use crate::types::{AccountId, InMemorySigner};
+use crate::types::{AccountId, InMemorySigner, Balance};
 use crate::{Network, Worker};
 
 use super::CallExecutionDetails;
@@ -21,6 +21,9 @@ impl Account {
         &self.signer
     }
 
+    /// Call a contract on the network specified within `worker`, and return
+    /// a Builder object that we will make use to populate the rest of the
+    /// call details.
     pub fn call<'a, T: Network>(
         &self,
         worker: &'a Worker<T>,
@@ -28,6 +31,32 @@ impl Account {
         function: String,
     ) -> CallBuilder<'a, T> {
         CallBuilder::new(worker, contract_id, self.signer.clone(), function)
+    }
+
+    /// Transfer near to an account specified by `receiver_id` with the amount
+    /// specified by `amount_yocto`. Returns the execution details of this
+    /// transaction
+    pub async fn transfer_near<T: Network>(
+        &self,
+        worker: &Worker<T>,
+        receiver_id: AccountId,
+        amount_yocto: Balance,
+    ) -> anyhow::Result<CallExecutionDetails> {
+        worker
+            .transfer_near(self.signer(), receiver_id, amount_yocto)
+            .await
+    }
+
+    /// Deletes the current account, and returns the execution details of this
+    /// transaction. The beneciary will receive the funds of the account deleted
+    pub async fn delete_account<T: Network>(
+        self,
+        worker: &Worker<T>,
+        beneficiary_id: AccountId,
+    ) -> anyhow::Result<CallExecutionDetails> {
+        worker
+            .delete_account(self.id, &self.signer, beneficiary_id)
+            .await
     }
 }
 
@@ -52,17 +81,48 @@ impl Contract {
         &self.account.id
     }
 
+    pub fn as_account(&self) -> &Account {
+        &self.account
+    }
+
     pub(crate) fn signer(&self) -> &InMemorySigner {
         self.account.signer()
     }
 
+    /// Call the current contract's function using the contract's own account
+    /// details to do the signing. Returns a Builder object that we will make
+    /// use to populate the rest of the call details.
+    ///
+    /// If we want to make use of the contract's account to call into a
+    /// different contract besides the current one, use
+    /// `contract.as_account().call` instead.
     pub fn call<'a, T: Network>(
         &self,
         worker: &'a Worker<T>,
-        other: AccountId,
         function: String,
     ) -> CallBuilder<'a, T> {
-        self.account.call(worker, other, function)
+        self.account.call(worker, self.id().clone(), function)
+    }
+
+    /// Call a view function into the current contract. Returns a result that
+    /// yields a JSON string object.
+    pub async fn view<T: Network>(
+        &self,
+        worker: &Worker<T>,
+        function: String,
+        args: Vec<u8>,
+    ) -> anyhow::Result<String> {
+        worker.view(self.id().clone(), function, args).await
+    }
+
+    /// Deletes the current contract, and returns the execution details of this
+    /// transaction. The beneciary will receive the funds of the account deleted
+    pub async fn delete_contract<T: Network>(
+        self,
+        worker: &Worker<T>,
+        beneficiary_id: AccountId,
+    ) -> anyhow::Result<CallExecutionDetails> {
+        self.account.delete_account(worker, beneficiary_id).await
     }
 }
 
@@ -121,6 +181,19 @@ impl<'a, T: Network> CallBuilder<'a, T> {
                     .expect("required `with_args` to be specified as apart of Call"),
                 self.gas,
                 self.deposit,
+            )
+            .await
+            .map(Into::into)
+    }
+
+    pub async fn view(self) -> anyhow::Result<Vec<u8>> {
+        self.worker
+            .client()
+            .view(
+                self.contract_id,
+                self.function,
+                self.args
+                    .expect("required `with_args` to be specified as apart of View"),
             )
             .await
             .map(Into::into)
