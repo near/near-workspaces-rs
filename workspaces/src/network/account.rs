a8275d9d48a3cb83b0dc3963935aa68b06c71ceb
Phuong Nguyen <phuong@near.org>
2021-12-17 00:58:05 +0000

    Update deploy to not consume self

--- workspaces/src/network/account.rs
+++ workspaces/src/network/account.rs
@@ -86,10 +86,10 @@ impl Account {
         )
     }
 
-    /// Deploy contract code or WASM bytes to the account, consuming it
-    /// and yielding us a new [`Contract`] object.
-    pub async fn deploy<T: Network>(
-        self,
+    /// Deploy contract code or WASM bytes to the account, and return us a new
+    /// [`Contract`] object that we can use to interact with the contract.
+    pub async fn deploy<T: Network, U: AsRef<[u8]>>(
+        &self,
         worker: &Worker<T>,
         wasm: Vec<u8>,
     ) -> anyhow::Result<CallExecution<Contract>> {
@@ -99,7 +99,7 @@ impl Account {
             .await?;
 
         Ok(CallExecution {
-            result: Contract::account(self),
+            result: Contract::new(self.id().clone(), self.signer().clone()),
             details: outcome.into(),
         })
     }
