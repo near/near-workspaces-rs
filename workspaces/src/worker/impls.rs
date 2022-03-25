d0e56648b5d3b09a384a3d4ce48e5eda6fbf79d2
Phuong Nguyen <phuong@near.org>
2022-03-25 11:39:57 -0700

    Add docs sandbox specific functions

--- workspaces/src/worker/impls.rs
+++ workspaces/src/worker/impls.rs
@@ -164,6 +164,9 @@ impl Worker<Sandbox> {
         Account::new(account_id, signer)
     }
 
+    /// Import a contract from the the given network, and return us a [`ImportContractTransaction`]
+    /// which allows to specify further details, such as being able to import contract data and
+    /// how far back in time we wanna grab the contract.
     pub fn import_contract<'a, 'b>(
         &'b self,
         id: &AccountId,
@@ -172,6 +175,9 @@ impl Worker<Sandbox> {
         self.workspace.import_contract(id, worker)
     }
 
+    /// Patch state into the sandbox network, given a key and value. This will allow us to set
+    /// state that we have acquired in some manner. This allows us to test random cases that
+    /// are hard to come up naturally as state evolves.
     pub async fn patch_state(
         &self,
         contract_id: &AccountId,
@@ -181,6 +187,12 @@ impl Worker<Sandbox> {
         self.workspace.patch_state(contract_id, key, value).await
     }
 
+    /// Fast forward to a point in the future. The delta block height is supplied to tell the
+    /// network to advanced a certain amount of blocks. This comes with the advantage only having
+    /// to wait a fraction of the time it takes to produce the same number of blocks.
+    ///
+    /// Estimate as to how long it takes: if our delta_height crosses `X` epochs, then it would
+    /// roughly take `X * 5` seconds for the fast forward request to be processed.
     pub async fn fast_forward(&self, delta_height: u64) -> anyhow::Result<()> {
         self.workspace.fast_forward(delta_height).await
     }
