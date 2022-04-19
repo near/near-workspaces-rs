bda570d6402b6e0a8389a12113c7ea1cc15efda6
adsick <adsick@protonmail.com>
2022-04-19 16:55:50 +0300

    fix build.rs now using ensure_sandbox_bin() instead of installing it every time

--- workspaces/build.rs
+++ workspaces/build.rs
@@ -1,6 +1,9 @@
 fn main() {
     let doc_build = cfg!(doc) || std::env::var("DOCS_RS").is_ok();
     if !doc_build && cfg!(feature = "install") {
-        near_sandbox_utils::install().expect("Could not install sandbox");
+        // using unwrap because all the useful error messages are hidden inside
+        near_sandbox_utils::ensure_sandbox_bin().unwrap();
+        // previously the next line was causing near sandbox to be installed every time cargo build/check was called
+        // near_sandbox_utils::install().expect("Could not install sandbox");
     }
 }
