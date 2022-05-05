1c4c459bb7bab1b0dea52b48651ea865ea65d344
adsick <vadimgangsta73@gmail.com>
2022-05-05 23:23:14 +0300

    explanatory errors in build.rs

--- workspaces/build.rs
+++ workspaces/build.rs
@@ -1,6 +1,9 @@
 fn main() {
     let doc_build = cfg!(doc) || std::env::var("DOCS_RS").is_ok();
     if !doc_build && cfg!(feature = "install") {
-        near_sandbox_utils::ensure_sandbox_bin().expect("Could not install sandbox");
+        match near_sandbox_utils::ensure_sandbox_bin(){
+            Ok(p) => println!("Successfully installed sandbox in: {:?}", p),
+            Err(e) => panic!("Could not install sandbox\nReason: {:?}", e),
+        }
     }
 }
