25f7c040b5ff94bc1c8f3ae30cafdf741047dee0
austinabell <austinabell8@gmail.com>
2022-05-10 21:53:38 -0400

    fmt

--- workspaces/build.rs
+++ workspaces/build.rs
@@ -1,6 +1,7 @@
 fn main() {
     let doc_build = cfg!(doc) || std::env::var("DOCS_RS").is_ok();
     if !doc_build && cfg!(feature = "install") {
-        near_sandbox_utils::install_with_version("1.26.0/57b6ac92b0e77deade8a6eef1a322272511aa5de").expect("Could not install sandbox");
+        near_sandbox_utils::install_with_version("1.26.0/57b6ac92b0e77deade8a6eef1a322272511aa5de")
+            .expect("Could not install sandbox");
     }
 }
