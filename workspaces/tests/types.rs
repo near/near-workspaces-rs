a039d06f4bca1618e2a1e285811f1b8be22a7e42
Phuong Nguyen <ChaoticTempest@users.noreply.github.com>
2022-05-16 10:05:33 -0700

    chore: add secret key creation methods and added new KeyType type (#139)
    * Add secret key creation methods
    
    * Fix up KeyType being used elsewhere
    
    * Expose types module
    
    * Added tests for keypair creation

---
+++ workspaces/tests/types.rs
@@ -0,0 +1,31 @@
+use workspaces::types::{KeyType, SecretKey};
+
+#[test]
+fn test_keypair_ed25519() -> anyhow::Result<()> {
+    let pk_expected = "\"ed25519:DcA2MzgpJbrUATQLLceocVckhhAqrkingax4oJ9kZ847\"";
+    let sk_expected = "\"ed25519:3KyUuch8pYP47krBq4DosFEVBMR5wDTMQ8AThzM8kAEcBQEpsPdYTZ2FPX5ZnSoLrerjwg66hwwJaW1wHzprd5k3\"";
+
+    let sk = SecretKey::from_seed(KeyType::ED25519, "test");
+    let pk = sk.public_key();
+    assert_eq!(serde_json::to_string(&pk)?, pk_expected);
+    assert_eq!(serde_json::to_string(&sk)?, sk_expected);
+    assert_eq!(pk, serde_json::from_str(pk_expected)?);
+    assert_eq!(sk, serde_json::from_str(sk_expected)?);
+
+    Ok(())
+}
+
+#[test]
+fn test_keypair_secp256k1() -> anyhow::Result<()> {
+    let pk_expected = "\"secp256k1:BtJtBjukUQbcipnS78adSwUKE38sdHnk7pTNZH7miGXfodzUunaAcvY43y37nm7AKbcTQycvdgUzFNWsd7dgPZZ\"";
+    let sk_expected = "\"secp256k1:9ZNzLxNff6ohoFFGkbfMBAFpZgD7EPoWeiuTpPAeeMRV\"";
+
+    let sk = SecretKey::from_seed(KeyType::SECP256K1, "test");
+    let pk = sk.public_key();
+    assert_eq!(serde_json::to_string(&pk)?, pk_expected);
+    assert_eq!(serde_json::to_string(&sk)?, sk_expected);
+    assert_eq!(pk, serde_json::from_str(pk_expected)?);
+    assert_eq!(sk, serde_json::from_str(sk_expected)?);
+
+    Ok(())
+}
