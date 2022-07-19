4d209ea3468c099e948ef43fb05ac8da54dd5c2d
Phuong Nguyen <phuong@near.org>
2022-07-19 12:29:07 -0700

    Added rerun to build.rs script

--- workspaces/build.rs
+++ workspaces/build.rs
@@ -1,4 +1,6 @@
 fn main() {
+    println!("cargo:rerun-if-env-changed=NEAR_SANDBOX_BIN_PATH");
+
     let doc_build = cfg!(doc) || std::env::var("DOCS_RS").is_ok();
     let env_bin = std::env::var("NEAR_SANDBOX_BIN_PATH").is_ok();
     if !doc_build && !env_bin && cfg!(feature = "install") {
