d6bc23f4aa64cbb0d0af5abc0cc24296655ba42f
Phuong Nguyen <phuong@near.org>
2022-11-08 16:45:01 -0800

    Updated queries example

--- examples/src/various_queries.rs
+++ examples/src/various_queries.rs
@@ -1,10 +1,14 @@
+use serde_json::json;
+
+/// This example will show various calls into viewing information from what's on the sandbox chain
+/// to what was on the chain prior to modifying state.
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     let worker = workspaces::sandbox().await?;
 
     // Fetch the latest block produced from the network.
     let block = worker.view_block().await?;
-    println!("Block: {block:?}");
+    println!("Latest Block: {block:#?}");
 
     // Fetch the block from the genesis point of the sandbox network. This is not necessarily
     // the genesis of every network since they can re-genesis at a higher block height.
@@ -13,7 +17,7 @@ async fn main() -> anyhow::Result<()> {
         .block_height(0)
         // can instead use .block_hash(CryptoHash) as well
         .await?;
-    println!("Sandbox Geneis Block: {genesis_block:?}");
+    println!("Sandbox Geneis Block: {genesis_block:#?}");
 
     // Reference the chunk via the block hash we queried for earlier:
     let shard_id = 0;
@@ -21,7 +25,72 @@ async fn main() -> anyhow::Result<()> {
         .view_chunk()
         .block_hash_and_shard(*block.hash(), shard_id)
         .await?;
-    println!("Chunk: {chunk:?}");
+    println!("Latest Chunk: {chunk:#?}");
+
+    let bob = worker.dev_create_account().await?;
+    println!("\nCreated bob's account with id {:?}", bob.id());
+
+    // Show all the access keys relating to bob:
+    let access_keys = bob.view_access_keys().await?;
+    println!("bob's access keys: {access_keys:?}");
+
+    let status_msg = worker
+        .dev_deploy(include_bytes!("../res/status_message.wasm"))
+        .await?;
+
+    // Let's have bob set the "Hello" message into the contract.
+    let outcome = bob
+        .call(status_msg.id(), "set_status")
+        .args_json(json!({
+            "message": "Hello"
+        }))
+        .transact()
+        .await?
+        .into_result()?;
+    println!(
+        "Bob burnt {} gas callling into `set_status('Hello')`",
+        outcome.total_gas_burnt
+    );
+
+    // let's get a reference point to the chain at it's current state, so we can reference it back later
+    // when we want older data from the chain.
+    let block = worker.view_block().await?;
+
+    // Override bob's message of "Hello" with "World".
+    let outcome = bob
+        .call(status_msg.id(), "set_status")
+        .args_json(json!({
+            "message": "World"
+        }))
+        .transact()
+        .await?
+        .into_result()?;
+    println!(
+        "Bob burnt {} gas callling into `set_status('World')`",
+        outcome.total_gas_burnt
+    );
+
+    // Then view that it indeed has changed:
+    let msg: String = status_msg
+        .view("get_status")
+        .args_json(json!({
+            "account_id": bob.id(),
+        }))
+        .await?
+        .json()?;
+    println!("Bob's status message: '{}'", msg);
+
+    // But since we have a reference point to before bob overrode his message, we can view the message
+    // from then as well, by giving the reference into the view function call:
+    let msg: String = status_msg
+        .view("get_status")
+        .args_json(json!({
+            "account_id": bob.id(),
+        }))
+        .block_hash(*block.hash())
+        .await?
+        .json()?;
+    println!("Bob's older status message: '{}'", msg);
 
     Ok(())
 }
