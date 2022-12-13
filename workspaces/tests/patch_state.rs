8b2d2f1a7c9155f5a318ea072ff9cb9c059f9bef
Phuong Nguyen <phuong@near.org>
2022-12-12 17:48:42 -0800

    tmp

--- workspaces/tests/patch_state.rs
+++ workspaces/tests/patch_state.rs
@@ -4,7 +4,10 @@
 use borsh::{self, BorshDeserialize, BorshSerialize};
 use serde_json::json;
 use test_log::test;
-use workspaces::{AccountId, DevNetwork, Worker};
+use workspaces::{
+    types::{KeyType, SecretKey},
+    AccessKey, Account, AccountDetails, AccountId, DevNetwork, Worker,
+};
 
 const STATUS_MSG_WASM_FILEPATH: &str = "../examples/res/status_message.wasm";
 
@@ -83,6 +86,60 @@ async fn test_patch_state() -> anyhow::Result<()> {
         .json()?;
 
     assert_eq!(status, "hello world".to_string());
+
+    Ok(())
+}
+
+#[test(tokio::test)]
+async fn test_patch() -> anyhow::Result<()> {
+    let worker = workspaces::sandbox().await?;
+    let (contract_id, mut status_msg) = view_status_state(worker.clone()).await?;
+    status_msg.records.push(Record {
+        k: "alice.near".to_string(),
+        v: "hello world".to_string(),
+    });
+
+    worker
+        .patch(&contract_id)
+        .state(b"STATE", &status_msg.try_to_vec()?)
+        .transact()
+        .await?;
+
+    let status: String = worker
+        .view(&contract_id, "get_status")
+        .args_json(json!({
+            "account_id": "alice.near",
+        }))
+        .await?
+        .json()?;
+
+    assert_eq!(status, "hello world".to_string());
+
+    Ok(())
+}
+
+#[test(tokio::test)]
+async fn test_patch_keys() -> anyhow::Result<()> {
+    let worker = workspaces::sandbox().await?;
+    let (contract_id, mut status_msg) = view_status_state(worker.clone()).await?;
+
+    let sk = SecretKey::from_seed(KeyType::ED25519, "bob's test key");
+    let bob = "bob.test.near".parse()?;
+    let bob = Account::from_secret_key(bob, sk, &worker);
+
+    let code_hash = worker.view_account(&contract_id).await?.code_hash;
+
+    worker
+        .patch(bob.id())
+        // .account(AccountDetails {
+        //     balance: near_units::parse_near!("100 N"),
+        //     locked: 0,
+        //     code_hash,
+        //     storage_usage: 100000,
+        // })
+        .access_key(bob.secret_key().public_key(), AccessKey::full_access())
+        .transact()
+        .await?;
 
     Ok(())
 }
