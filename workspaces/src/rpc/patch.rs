7e47ec4ef2226b14821dc1f2174dadc7aa074496
Phuong Nguyen <phuong@near.org>
2022-12-17 14:21:35 -0800

    Remove prints

--- workspaces/src/rpc/patch.rs
+++ workspaces/src/rpc/patch.rs
@@ -148,29 +148,11 @@ impl<'a, 'b> ImportContractTransaction<'a> {
             .await
             .map_err(|err| SandboxErrorCode::PatchStateFailure.custom(err))?;
 
-        println!("PATCHING INTO: {account_id:?}");
         self.into_network
             .client()
             .query(&RpcSandboxPatchStateRequest { records })
             .await
             .map_err(|err| SandboxErrorCode::PatchStateFailure.custom(err))?;
-
-        for i in 0..20 {
-            let account_view = self
-                .into_network
-                .view_account(&account_id)
-                .block_reference(BlockReference::latest())
-                .await;
-
-            println!("Waiting on {account_id:?} to exist but got {account_view:?}. Retrying for the {i}th in 500ms...");
-            if account_view.is_err() {
-                tokio::time::sleep(std::time::Duration::from_millis(500)).await;
-            } else {
-                break;
-            }
-        }
-
-        // tokio::time::sleep(std::time::Duration::from_millis(3000)).await;
 
         Ok(Contract::new(signer, self.into_network))
     }
