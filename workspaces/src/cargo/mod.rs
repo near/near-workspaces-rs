446720cee533dd8cf338e3513bf4b06e5a78f506
ghimire007 <ghimireprakash778@gmail.com>
2023-06-17 18:30:56 +0545

    enhancement : use cargo near to build project

--- workspaces/src/cargo/mod.rs
+++ workspaces/src/cargo/mod.rs
@@ -8,9 +8,17 @@ use std::fs;
 use std::io;
 use std::path::Path;
 use std::process::Stdio;
+use std::convert::TryInto;
 
 use crate::error::ErrorKind;
 use crate::Result;
+
+
+use cargo_near::build::run;
+use cargo_near::{BuildCommand, ColorPreference};
+use cargo_near::util::CompilationArtifact;
+
+
 
 fn cargo_bin() -> Command {
     match env::var_os("CARGO") {
@@ -33,53 +41,30 @@ fn root_cargo_metadata() -> Result<Metadata> {
     })
 }
 
-async fn build_cargo_project<P: AsRef<Path> + Debug>(project_path: P) -> Result<Vec<Message>> {
-    let metadata = root_cargo_metadata()?;
-    let output = cargo_bin()
-        .args([
-            "build",
-            "--target",
-            "wasm32-unknown-unknown",
-            "--release",
-            "--message-format=json",
-            "--target-dir",
-            metadata.target_directory.as_str(),
-        ])
-        .current_dir(&project_path)
-        .stdout(Stdio::piped())
-        .stderr(Stdio::piped())
-        .output()
-        .await
-        .map_err(|e| ErrorKind::Io.custom(e))?;
-
-    debug!(
-        target: "workspaces",
-        "Building project '{:?}' resulted in status {:?}",
-        &project_path, output.status
-    );
-
-    if output.status.success() {
-        let reader = std::io::BufReader::new(output.stdout.as_slice());
-        Ok(Message::parse_stream(reader).map(|m| m.unwrap()).collect())
-    } else {
-        Err(ErrorKind::Io.message(format!(
-            "Failed to build project '{:?}'.\n\
-            Stderr:\n\
-            {}\n\
-            Stdout:\n\
-            {}",
-            project_path,
-            String::from_utf8(output.stderr).map_err(|e| ErrorKind::DataConversion.custom(e))?,
-            String::from_utf8(output.stdout).map_err(|e| ErrorKind::DataConversion.custom(e))?,
-        )))
-    }
+async fn build_cargo_project<P: AsRef<Path> + Debug>(project_path: P) -> Result<CompilationArtifact>
+{
+    let  manifest_path = project_path.as_ref().join("Cargo.toml");
+    let command= BuildCommand{
+        release:true,
+        embed_abi:true,
+        doc:false,
+        color:ColorPreference::Always,
+        no_abi:true,
+        out_dir:None,
+        manifest_path:Some(manifest_path.try_into()
+        .map_err(|e| ErrorKind::Io.custom(e))?
+    )
+    };
+    let result=run(command).map_err(|e| ErrorKind::Io.custom(e))?;
+    Ok(result)
 }
 
 /// Builds the cargo project located at `project_path` and returns the generated wasm file contents.
 ///
 /// NOTE: This function does not check whether the resulting wasm file is a valid smart
 /// contract or not.
-pub async fn compile_project(project_path: &str) -> Result<Vec<u8>> {
+pub async fn compile_project(project_path: &str) -> Result<Vec<u8>> 
+{
     let project_path = fs::canonicalize(project_path).map_err(|e| match e.kind() {
         io::ErrorKind::NotFound => ErrorKind::Io.message(format!(
             "Incorrect file supplied to compile_project('{}')",
@@ -87,43 +72,8 @@ pub async fn compile_project(project_path: &str) -> Result<Vec<u8>> {
         )),
         _ => ErrorKind::Io.custom(e),
     })?;
-    let messages = build_cargo_project(project_path).await?;
-
-    // We find the last compiler artifact message which should contain information about the
-    // resulting .wasm file
-    let compile_artifact = messages
-        .iter()
-        .filter_map(|m| match m {
-            cargo_metadata::Message::CompilerArtifact(artifact) => Some(artifact),
-            _ => None,
-        })
-        .last()
-        .ok_or_else(|| {
-            ErrorKind::Io.message(
-                "Cargo failed to produce any compilation artifacts. \
-                 Please check that your project contains a NEAR smart contract.",
-            )
-        })?;
-    // The project could have generated many auxiliary files, we are only interested in .wasm files
-    let wasm_files = compile_artifact
-        .filenames
-        .iter()
-        .filter(|f| f.as_str().ends_with(".wasm"))
-        .collect::<Vec<_>>();
-    match wasm_files.as_slice() {
-        [] => Err(ErrorKind::Io.message(
-            "Compilation resulted in no '.wasm' target files. \
-                 Please check that your project contains a NEAR smart contract.",
-        )),
-        [file] => {
-            let file = file.canonicalize().map_err(|e| ErrorKind::Io.custom(e))?;
-            Ok(tokio::fs::read(file)
-                .await
-                .map_err(|e| ErrorKind::Io.custom(e))?)
-        }
-        _ => Err(ErrorKind::Io.message(format!(
-            "Compilation resulted in more than one '.wasm' target file: {:?}",
-            wasm_files
-        ))),
-    }
+    let compile_artifact = build_cargo_project(project_path).await?;
+    let file = compile_artifact.path.canonicalize().map_err(|e| ErrorKind::Io.custom(e))?;
+    Ok(tokio::fs::read(file).await.map_err(|e| ErrorKind::Io.custom(e))?)
+  
 }
