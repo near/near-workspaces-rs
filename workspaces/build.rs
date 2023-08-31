2f9d80c7f9d2844b8c9f19aaa302cbc395c21f65
Yasir Shariff <yasir.shariff.a@gmail.com>
2023-08-31 21:02:42 +0300

    fix: commit hash comment

--- workspaces/build.rs
+++ workspaces/build.rs
@@ -5,7 +5,7 @@ fn main() {
     let env_bin = std::env::var("NEAR_SANDBOX_BIN_PATH").is_ok();
     if !doc_build && !env_bin && cfg!(feature = "install") {
         // TODO Update commit to stable version once binaries are published correctly
-        // Commit: https://github.com/near/nearcore/commit/dce4c514f60a7f7981f00290268af4faf7eec55c
+        // Commit: https://github.com/near/nearcore/commit/eb2bbe1c3f51912c04462ce988aa496fab03d60e
         near_sandbox_utils::install_with_version("master/eb2bbe1c3f51912c04462ce988aa496fab03d60e")
             .expect("Could not install sandbox");
     }
