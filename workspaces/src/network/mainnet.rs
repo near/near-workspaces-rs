4f82448a1f88a446d144e84bb56118a0f90c01bf
Yasir Shariff <yasir.shariff.a@gmail.com>
2023-09-11 14:16:45 +0300

    upd: add TLA creator to mainnet

--- workspaces/src/network/mainnet.rs
+++ workspaces/src/network/mainnet.rs
@@ -1,15 +1,24 @@
 use crate::network::{Info, NetworkClient, NetworkInfo};
-use crate::result::Result;
+use crate::result::{Execution, ExecutionFinalResult, Result};
 use crate::rpc::client::Client;
+use crate::types::{Balance, SecretKey};
+use crate::{Account, Contract, InMemorySigner, Network, Worker};
 use std::path::PathBuf;
 
+use async_trait::async_trait;
+
 use super::builder::{FromNetworkBuilder, NetworkBuilder};
+use super::TopLevelAccountCreator;
+use near_account_id::AccountId;
 
 /// URL to the mainnet RPC node provided by near.org.
 pub const RPC_URL: &str = "https://rpc.mainnet.near.org";
 
 /// URL to the mainnet archival RPC node provided by near.org.
 pub const ARCHIVAL_URL: &str = "https://archival-rpc.mainnet.near.org";
+
+/// Deposit balance for creating a new account on mainnet.
+const DEFAULT_DEPOSIT: Balance = 0;
 
 /// Mainnet related configuration for interacting with mainnet. Look at
 /// [`workspaces::mainnet`] and [`workspaces::mainnet_archival`] for how to
@@ -63,5 +72,60 @@ impl NetworkClient for Mainnet {
 impl NetworkInfo for Mainnet {
     fn info(&self) -> &Info {
         &self.info
+    }
+}
+
+/// The methods assume the account credentials are already present in the
+/// keystore. If not, you can use the [`Account::create_subaccount`] which also
+/// provide better configuration options.
+#[async_trait]
+impl TopLevelAccountCreator for Mainnet {
+    async fn create_tla(
+        &self,
+        worker: Worker<dyn Network>,
+        id: AccountId,
+        sk: SecretKey,
+    ) -> Result<Execution<Account>> {
+        let root_signer =
+            InMemorySigner::from_file(worker.workspace.info().keystore_path.as_path())?;
+
+        let outcome = self
+            .client()
+            .create_account(&root_signer, &id, sk.public_key(), DEFAULT_DEPOSIT) //
+            .await?;
+
+        let signer = InMemorySigner::from_secret_key(id, sk);
+        Ok(Execution {
+            result: Account::new(signer, worker),
+            details: ExecutionFinalResult::from_view(outcome),
+        })
+    }
+
+    async fn create_tla_and_deploy(
+        &self,
+        worker: Worker<dyn Network>,
+        id: AccountId,
+        sk: SecretKey,
+        wasm: &[u8],
+    ) -> Result<Execution<Contract>> {
+        let root_signer =
+            InMemorySigner::from_file(worker.workspace.info().keystore_path.as_path())?;
+
+        let outcome = self
+            .client()
+            .create_account_and_deploy(
+                &root_signer,
+                &id,
+                sk.public_key(),
+                DEFAULT_DEPOSIT,
+                wasm.into(),
+            )
+            .await?;
+
+        let signer = InMemorySigner::from_secret_key(id, sk);
+        Ok(Execution {
+            result: Contract::new(signer, worker),
+            details: ExecutionFinalResult::from_view(outcome),
+        })
     }
 }
