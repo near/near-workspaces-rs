2de28b65571ea0ed19482cee19dcdac00d467a16
Yasir Shariff <yasir.shariff.a@gmail.com>
2023-09-14 14:57:06 +0300

    fix: incl. gen file

---
+++ examples/src/gen/adder.rs
@@ -0,0 +1,10 @@
+pub type Pair = Vec<i64>;
+pub struct AbiClient {
+    pub contract: workspaces::Contract,
+}
+impl AbiClient {
+    pub async fn add(&self, a: Pair, b: Pair) -> anyhow::Result<Pair> {
+        let result = self.contract.call("add").args_json([a, b]).view().await?;
+        Ok(result.json::<Pair>()?)
+    }
+}
