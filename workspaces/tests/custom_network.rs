ac3760c10d5651e4d8143a1730b0c41024db6d55
Yasir Shariff <yasir.shariff.a@gmail.com>
2023-09-18 07:47:53 +0300

    upd: test

--- workspaces/tests/custom_network.rs
+++ workspaces/tests/custom_network.rs
@@ -3,11 +3,14 @@ pub const PAGODA_TESTNET_RPC_URL: &str = "https://near-testnet.api.pagoda.co/rpc
 
 #[tokio::test]
 async fn test_custom_network() -> anyhow::Result<()> {
+    // `NEAR_RPC_API_KEY="xxxx" cargo test --package workspaces --test custom_network -- test_custom_network --exact --nocapture`
     if std::env::var("NEAR_RPC_API_KEY").is_err() {
         // skip the test
+        println!("NEAR_RPC_API_KEY is not set, skipping the test");
         return Ok(());
     }
 
+    // Reference to what can be called by this network: https://docs.pagoda.co/endpoints
     let worker = workspaces::custom(PAGODA_TESTNET_RPC_URL).await?;
     let res = worker.view_block().await?;
 
