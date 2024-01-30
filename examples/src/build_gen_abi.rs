5add8023bdd0f9ff7fb3faa2e876b711d0920ff2
Yasir Shariff <yasir.shariff.a@gmail.com>
2024-01-30 20:01:02 +0300

    upd: docs & tidying

---
+++ examples/src/build_gen_abi.rs
@@ -0,0 +1,28 @@
+// This example shows how to use the `near_abi_client` Generation Based API.
+// We are generating client code using the schema for the ABI and and `workspaces-rs` to call into the contract.
+// More information about usage can be found here: <https://github.com/near/near-abi-client-rs/blob/main/README.md>
+//
+// A good scenario for usage might be when you are interacting with a contract or multiple contracts at an automated level
+// and you want to have a type-safe way of interacting with them.
+
+/// The generated api requires setup in the `build.rs` file to generate the client code.
+#[path = "gen/adder.rs"]
+mod generation_adder;
+
+const ADDER_WASM_FILEPATH: &str = "./examples/res/adder.wasm";
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let worker = near_workspaces::sandbox().await?;
+    let wasm = std::fs::read(ADDER_WASM_FILEPATH)?;
+    let contract = worker.dev_deploy(&wasm).await?;
+
+    // The client is initialized with the contract.
+    let abi_client = generation_adder::AbiClient { contract };
+
+    // Here we can call the method, now typed with arguments and return types.
+    let res = abi_client.add(vec![1, 2], vec![3, 4]).await?;
+
+    assert_eq!(res, [4, 6]);
+    Ok(())
+}
--- examples/src/view_abi.rs
+++ examples/src/view_abi.rs
@@ -1,44 +1,21 @@
-use std::convert::TryInto;
+// use std::convert::TryInto;
 
-const ADDER_WASM_FILEPATH: &str = "./examples/res/adder.wasm";
 
-#[tokio::main]
-async fn main() -> anyhow::Result<()> {
-    let worker = near_workspaces::sandbox().await?;
-    let wasm = std::fs::read(ADDER_WASM_FILEPATH)?;
-    let contract = worker.dev_deploy(&wasm).await?;
 
-    macro_run(contract.clone()).await?;
-    generation_run(contract).await?;
 
-    Ok(())
-}
 
-/// This part of the example uses the Macro API to get a client and use it.
-mod macro_adder {
-    near_workspaces::near_abi_client::generate!(Client for "../examples/res/adder.json");
-}
 
-pub async fn macro_run(contract: near_workspaces::Contract) -> anyhow::Result<()> {
-    let contract = macro_adder::Client { contract };
-    let res = contract.add(vec![1, 2], vec![3, 4]).await?;
 
-    let res = (res[0].try_into().unwrap(), res[1].try_into().unwrap());
-    assert_eq!(res, (4, 6));
+// /// This part of the example uses the Generation API to generate a client and use it.
+// #[path = "gen/adder.rs"]
+// mod generation_adder;
 
-    Ok(())
-}
+// pub async fn generation_run(contract: near_workspaces::Contract) -> anyhow::Result<()> {
+//     let contract = generation_adder::AbiClient { contract };
+//     let res = contract.add(vec![1, 2], vec![3, 4]).await?;
 
-/// This part of the example uses the Generation API to generate a client and use it.
-#[path = "gen/adder.rs"]
-mod generation_adder;
+//     let res = (res[0].try_into().unwrap(), res[1].try_into().unwrap());
+//     assert_eq!(res, (4, 6));
 
-pub async fn generation_run(contract: near_workspaces::Contract) -> anyhow::Result<()> {
-    let contract = generation_adder::AbiClient { contract };
-    let res = contract.add(vec![1, 2], vec![3, 4]).await?;
-
-    let res = (res[0].try_into().unwrap(), res[1].try_into().unwrap());
-    assert_eq!(res, (4, 6));
-
-    Ok(())
-}
+//     Ok(())
+// }
