60b17d82cb227bddb3c8d5f2f56760b020807776
Xiangyi Zheng <xiangyi@near.org>
2024-06-05 16:25:04 +0800

    make  from_builder_with_version pub

--- workspaces/src/network/sandbox.rs
+++ workspaces/src/network/sandbox.rs
@@ -45,7 +45,7 @@ impl Sandbox {
             )),
         }
     }
-    pub(crate) async fn from_builder_with_version<'a>(
+    pub async fn from_builder_with_version<'a>(
         build: NetworkBuilder<'a, Self>,
         version: &str,
     ) -> Result<Self> {
