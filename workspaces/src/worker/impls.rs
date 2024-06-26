07b55f13e0571585c1b12d8e351db0f381e762c5
Vlad Frolov <frolvlad@gmail.com>
2024-06-26 16:17:10 +0200

    removed 'unstable' feature flag from 'status' API method since it is fairly stable

--- workspaces/src/worker/impls.rs
+++ workspaces/src/worker/impls.rs
@@ -196,10 +196,6 @@ where
     }
 
     /// Returns the status of the network.
-    ///
-    /// TODO: Remove `unstable` feature flag once [`StatusResponse`] is stable.
-    /// See here: <https://github.com/near/nearcore/blob/27901ccce9c2300f9fe934f152493a95e05af316/core/primitives/src/views.rs#L678>
-    #[cfg(feature = "unstable")]
     pub async fn status(&self) -> Result<StatusResponse> {
         self.client().status().await
     }
