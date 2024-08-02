445e86e4e73fc044962228c752fa4fc3493d24a7
jaswinder6991 <jassmultani2@gmail.com>
2024-08-02 16:02:12 +0530

    chore: fixed clippy error.

--- workspaces/tests/nonce_cache.rs
+++ workspaces/tests/nonce_cache.rs
@@ -16,10 +16,10 @@ async fn test_nonce_caching_parallel() -> anyhow::Result<()> {
 
     // Prepare a large number of transactions to test caching
     const NUM_TRANSACTIONS: usize = 50;
-    let messages: Vec<String> = (0..NUM_TRANSACTIONS).map(|i| format!("msg{}", i)).collect();
 
     // Send transactions in parallel
-    let parallel_tasks = messages.into_iter().map(|msg| {
+    let parallel_tasks = (0..NUM_TRANSACTIONS).map(|i| {
+        let msg = format!("msg{}", i);
         let account = account.clone();
         let contract_id = contract.id().clone();
         tokio::spawn(async move {
@@ -34,6 +34,7 @@ async fn test_nonce_caching_parallel() -> anyhow::Result<()> {
     // Collect all transaction statuses
     let statuses = futures::future::join_all(parallel_tasks).await;
 
+    // Wait for all transactions to complete
     for status in statuses {
         let status = status??;
         loop {
