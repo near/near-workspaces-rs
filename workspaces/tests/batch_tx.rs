7abadd69ca70055f27597b50ede76daaa19d0ac0
AlexKushnir1 <alex@alex-Precision-7510>
2024-09-02 18:16:00 +0300

    separate two calls

--- workspaces/tests/batch_tx.rs
+++ workspaces/tests/batch_tx.rs
@@ -21,13 +21,15 @@ async fn test_batch_tx() -> anyhow::Result<()> {
                 }))
                 .deposit(NearToken::from_near(0)),
         )
-        .call(
-            Function::new("set_status")
-                .args_json(json!({
-                    "message": "234325435",
-                }))
-                .deposit(NearToken::from_near(0)),
-        )
+        .transact()
+        .await?
+        .into_result()?;
+
+    contract
+        .batch()
+        .call(Function::new("set_status").args_json(json!({
+            "message": "world_hello",
+        })))
         .transact()
         .await?
         .into_result()?;
