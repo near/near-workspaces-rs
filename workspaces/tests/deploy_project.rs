c867cae3569b6e06b5f10fdff20aec4984623e21
dj8yf0μl <noreply@nowhere.org>
2025-05-16 17:03:41 +0300

    ci: revert back to stable; install second toolchain for 1 test

--- workspaces/tests/deploy_project.rs
+++ workspaces/tests/deploy_project.rs
@@ -1,11 +1,33 @@
 #![cfg(feature = "unstable")]
 #![recursion_limit = "256"]
+use near_workspaces::{
+    cargo_near_build as near_build,
+    error::{Error, ErrorKind},
+};
 use test_log::test;
 
 #[test(tokio::test)]
 async fn test_dev_deploy_project() -> anyhow::Result<()> {
     let worker = near_workspaces::sandbox().await?;
-    let wasm = near_workspaces::compile_project("./tests/test-contracts/status-message").await?;
+    // TODO: uncomment back when nearcore becomes compatible with rust 1.87
+    // let wasm = near_workspaces::compile_project("./tests/test-contracts/status-message").await?;
+    let wasm = {
+        let build_opts = cargo_near_build::BuildOpts::builder()
+            .no_locked(true)
+            .manifest_path(
+                cargo_near_build::camino::Utf8PathBuf::from(
+                    "./tests/test-contracts/status-message",
+                )
+                .join("Cargo.toml"),
+            )
+            .override_toolchain("1.86.0")
+            .build();
+        let compile_artifact = near_build::build_with_cli(build_opts)
+            .map_err(|e| Error::custom(ErrorKind::Other, e))?;
+        let file = compile_artifact.canonicalize()?;
+        std::fs::read(file)?
+    };
+
     let contract = worker.dev_deploy(&wasm).await?;
 
     contract
