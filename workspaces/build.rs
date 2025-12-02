030285f799205e8fb1c941bbffc659c732b1fab3
Vladyslav Savchyn <vsavchyn2005@gmail.com>
2025-12-02 18:06:21 -0500

    chore: migration to `near-sandbox-rs`

--- workspaces/build.rs
+++ workspaces/build.rs
@@ -4,6 +4,6 @@ fn main() {
     let doc_build = cfg!(doc) || std::env::var("DOCS_RS").is_ok();
     let env_bin = std::env::var("NEAR_SANDBOX_BIN_PATH").is_ok();
     if !doc_build && !env_bin && cfg!(feature = "install") {
-        near_sandbox_utils::install().expect("Could not install sandbox");
+        near_sandbox::install().expect("Could not install sandbox");
     }
 }
